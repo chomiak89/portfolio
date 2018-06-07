118f12a52562ba197e53773917fdc993f8d21329
Kamil Chomiak <kchomiak@outlook.com>
2018-06-06 19:30:01 -0500

    created header and started main content

---
+++ js/wdthdspl.js
@@ -0,0 +1,11 @@
+const updateWidth = document.getElementById('updateWidth');
+
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+
+function displayWindowSize() {
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+        // your size calculation code here
+        document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
+    };
